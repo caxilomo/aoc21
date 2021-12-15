448376b3035634820aad402d2edfef93a9dfc23c
caxilomo <3996185+caxilomo@users.noreply.github.com>
2021-12-15 12:50:53 +0100

    Day 2: Part 2

--- day-2-dive/main.py
+++ day-2-dive/main.py
@@ -1,13 +1,15 @@
 import os
 import re
+import string
+from typing import Tuple
 
 
 def get_steps(patter, command) -> int:
-    forward_steps = re.findall(patter, command)
-    if not forward_steps:
+    steps = re.findall(patter, command)
+    if not steps:
         return 0
 
-    return int(forward_steps[0])
+    return int(steps[0])
 
 
 def get_forward_steps(command) -> int:
@@ -22,6 +24,32 @@ def get_up_steps(command) -> int:
     return get_steps("up (\d)+", command)
 
 
+def part_1(values: [string]) -> Tuple[int, int, int]:
+    position = 0
+    depth = 0
+    for value in values:
+        position += get_forward_steps(value)
+        depth += get_down_steps(value)
+        depth -= get_up_steps(value)
+
+
+    return position, depth, position * depth
+
+
+def part_2(values: [string]) -> Tuple[int, int, int]:
+    position = 0
+    depth = 0
+    aim = 0
+
+    for value in values:
+        position += get_forward_steps(value)
+        depth += aim * get_forward_steps(value)
+        aim += get_down_steps(value)
+        aim -= get_up_steps(value)
+
+    return position, depth, position * depth
+
+
 script_dir = os.path.dirname(__file__)
 rel_path = "input.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
@@ -31,12 +59,5 @@ with open(abs_file_path) as inputFile:
 
 inputFile.close()
 
-position = 0
-depth = 0
-
-for command in commands:
-    position += get_forward_steps(command)
-    depth += get_down_steps(command)
-    depth -= get_up_steps(command)
-
-print(position, depth, position * depth)
+print(part_1(commands))
+print(part_2(commands))
